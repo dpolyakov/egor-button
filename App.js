b7cc9ee13f4ad4f9a877e792855c13b8da83881f
Dmitry Polyakov <dima@my-html.ru>
2023-12-09 13:59:27 +0300

    v0.0.1

--- App.js
+++ App.js
@@ -1,20 +1,115 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Audio } from 'expo-av';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useAssets } from 'expo-asset';
+import {ThemedButton} from "react-native-really-awesome-button";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <LinearGradient
+            colors={['#0042D9', '#0093FF', '#0078FF']}
+            style={styles.background}
+        >
+            <PlayButton />
+        </LinearGradient>
+
     </View>
   );
 }
 
+
+const PlayButton = () => {
+    const [sound, setSound] = React.useState();
+    const [isPlaying, setPlaying] = React.useState(false);
+    const assetsList = [
+        require('./assets/1.wav'),
+        require('./assets/2.wav'),
+        require('./assets/3.wav'),
+    ]
+    const [soundAssets] = useAssets(assetsList)
+
+    const getSound = () => {
+
+        console.log({soundAssets})
+        const index = Math.floor(Math.random() * (assetsList.length) + 0);
+
+        return soundAssets[index];
+    }
+
+    async function playSound() {
+        console.log('Loading Sound', getSound());
+        const soundAsset = getSound();
+        const { sound } = await Audio.Sound.createAsync(soundAsset);
+        setSound(sound);
+
+        console.log('Playing Sound');
+        try {
+            setPlaying(true)
+            await sound.playAsync().then(({isPlaying}) => {
+                setPlaying(isPlaying)
+            });
+        } catch (error) {
+            setPlaying(false)
+            console.error(error)
+        }
+
+      }
+
+    React.useEffect(() => {
+    return sound
+        ? () => {
+            console.log('Unloading Sound');
+            sound.unloadAsync();
+        }
+        : undefined;
+    }, [sound]);
+
+    return (
+        <View style={styles.playButton}>
+            <ThemedButton
+                name="cartman"
+                type="secondary"
+                height={200}
+                borderRadius={200}
+                onPress={playSound}
+                disabled={isPlaying}
+                raiseLevel={5}
+                >
+                Жми
+            </ThemedButton>
+        </View>
+    )
+}
+
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: '100%',
+  },
+  playButton: {
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    marginLeft: '-25%',
+    marginTop: -100
+  },
+  image_shadows: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 10,
+    height: 280
+  }
 });
